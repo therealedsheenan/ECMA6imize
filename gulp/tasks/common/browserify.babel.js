f5345f802444b19d8fe614dd74e4d16662856258
Sheenan Tenepre <sheenan.tenepre@shiftbrain.co.jp>
2016-01-15 18:54:07 +0900

    clean css files and add little documentation

--- gulp/tasks/common/browserify.babel.js
+++ gulp/tasks/common/browserify.babel.js
@@ -14,7 +14,6 @@ let bundleAll = () => {
     let bundler = watchify(browserify(config.browserify.src, { debug: true }).transform(babel));
     let rebundle = () => {
         bundler
-
             .bundle()
             .on('error', handleErrors)
             .pipe(source('main.js'))
