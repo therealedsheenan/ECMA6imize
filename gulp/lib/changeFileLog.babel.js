5e3d9821a36eeed46ea4e0d18c49ed613a0c14c6
Sheenan Tenepre <sheenan.tenepre@shiftbrain.co.jp>
2016-02-14 22:38:45 +0900

    brushup

--- gulp/lib/changeFileLog.babel.js
+++ gulp/lib/changeFileLog.babel.js
@@ -3,7 +3,10 @@ import gutil from 'gulp-util'
 
 let changeFileLog = ( filePath, msg ) => {
     if ( msg == null ) msg = ''
-    return gutil.log(gutil.colors.green(path.relative( process.cwd(), filePath )) + ' ' + gutil.colors.blue( msg ))
+    return
+        gutil.log(
+            gutil.colors.green(
+                path.relative( process.cwd(), filePath )) + ' ' + gutil.colors.blue( msg ))
 }
 
 export default changeFileLog
