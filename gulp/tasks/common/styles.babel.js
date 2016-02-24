d1b4fdf4c7a59d86f9d9a0864b61b88653c78ad5
Sheenan Tenepre <sheenan.tenepre@shiftbrain.co.jp>
2016-02-24 17:34:41 +0900

    add css lint

--- gulp/tasks/common/styles.babel.js
+++ gulp/tasks/common/styles.babel.js
@@ -9,21 +9,46 @@ import autoprefixer from 'autoprefixer'
 import cssnext from 'postcss-cssnext'
 import nested from 'postcss-nested'
 import atImport from 'postcss-import'
+import reporter from 'postcss-reporter'
+import stylelint from 'stylelint'
 import mqpacker from 'css-mqpacker'
 import minify from 'cssnano'
 import rename from 'gulp-rename'
 import sass from 'gulp-ruby-sass'
+import syntax_scss from 'postcss-scss'
 import gulpif from 'gulp-if'
 import browserSync from 'browser-sync'
 import plumber from 'gulp-plumber'
 
 const dest = gulp.dest
 const dev = env
+const stylelintConfig = {
+    "rules": {
+    "block-no-empty": true,
+    "color-no-invalid-hex": true,
+    "declaration-colon-space-after": "always",
+    "declaration-colon-space-before": "never",
+    "function-comma-space-after": "always",
+    "media-feature-colon-space-after": "always",
+    "media-feature-colon-space-before": "never",
+    "max-empty-lines": 5,
+    "number-no-trailing-zeros": true,
+    "declaration-block-no-single-line": true,
+    "rule-trailing-semicolon": "always",
+    "selector-list-comma-space-before": "never",
+    "selector-list-comma-newline-after": "always"
+    }
+}
 
 const cssTools = [
     atImport,
     cssnext,
     nested,
+    stylelint( stylelintConfig ),
+    reporter({
+      clearMessages: true,
+      throwError: true
+    }),
     autoprefixer( config.styles.autoprefixer ),
     mqpacker
 ]
@@ -45,7 +70,7 @@ let generateStyles = () => {
     return sass(styleFile, options)
         .pipe(plumber())
         .on('error', handleErrors)
-        .pipe(postcss( cssTools ))
+        .pipe(postcss( cssTools ), { syntax: syntax_scss })
         .pipe(gulpif(dev == config.build.prod, postcss( min )))
         .pipe(gulpif(dev == config.build.dev, sourcemaps.write()))
         .pipe(rename( renameFile ))
