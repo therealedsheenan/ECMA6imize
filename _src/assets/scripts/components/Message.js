36cf0072cd80080b9b33f3d35f97669db530e292
Sheenan Tenepre <sheenan.tenepre@shiftbrain.co.jp>
2016-03-04 10:32:48 +0900

    update vendor scripts and components

---
+++ _src/assets/scripts/components/Message.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+class Message extends React.Component {
+    render () {
+        return (
+            <div className="Message-inner">
+                <h1>ECMA6imize</h1>
+                <p>This is a site generator</p>
+            </div>
+        )
+    }
+}
+
+export default Message
