e7747ac33340f413458934a3b9b6bdc289862d05
Sheenan Tenepre <sheenan.tenepre@shiftbrain.co.jp>
2016-03-28 13:56:21 +0900

    add css lint configs

---
+++ gulp/lib/styleLintConfig.babel.js
@@ -0,0 +1,19 @@
+const stylelintConfig = {
+    "rules": {
+    "block-no-empty": true,
+    "color-no-invalid-hex": true,
+    "declaration-colon-space-after": "always",
+    "declaration-colon-space-before": "never",
+    "function-comma-space-after": "always",
+    "media-feature-colon-space-after": "always",
+    "media-feature-colon-space-before": "never",
+    "max-empty-lines": 5,
+    "number-no-trailing-zeros": true,
+    "declaration-block-no-single-line": true,
+    "rule-trailing-semicolon": "always",
+    "selector-list-comma-space-before": "never",
+    "selector-list-comma-newline-after": "always"
+    }
+}
+
+export default stylelintConfig
