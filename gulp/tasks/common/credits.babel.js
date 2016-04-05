363d9e69b68479a39e20d928290745251b88df5b
Sheenan Tenepre <sheenan.tenepre@shiftbrain.co.jp>
2016-04-05 17:32:14 +0900

    add credits

---
+++ gulp/tasks/common/credits.babel.js
@@ -0,0 +1,15 @@
+import gulp from 'gulp'
+
+gulp.task('credits', () => {
+    console.log("                                         #        #               ");
+    console.log(" #######  #####  #     #    #     #####                           ");
+    console.log(" #       #     # ##   ##   # #   #     # # #    # # ###### ###### ");
+    console.log(" #       #       # # # #  #   #  #       # ##  ## #     #  #      ");
+    console.log(" #####   #       #  #  # #     # ######  # # ## # #    #   #####  ");
+    console.log(" #       #       #     # ####### #     # # #    # #   #    #      ");
+    console.log(" #       #     # #     # #     # #     # # #    # #  #     #      ");
+    console.log(" #######  #####  #     # #     #  #####  # #    # # ###### ###### ");
+    console.log("")
+    console.log("By: Sheenan Tenepre")
+    console.log("")
+})
