2096c7563de2ec5387b9c44f1ed26205fb9cdaae
Sheenan Tenepre <sheenantenepre@gmail.com>
2016-06-24 02:30:36 +0900

    updates

--- _src/assets/scripts/main.babel.js
+++ _src/assets/scripts/main.babel.js
@@ -1,8 +1,8 @@
-import React from 'react'
-import { render } from 'react-dom'
-import Message from './components/Message.js'
-import { Router, Route, IndexRoute, Link, hashHistory } from 'react-router'
-
-render (<Message />, document.getElementById('main'))
-
-console.log("Sheenan Tenepre's ECMA6imize");
+// import React from 'react'
+// import { render } from 'react-dom'
+// import Message from './components/Message.js'
+// import { Router, Route, IndexRoute, Link, hashHistory } from 'react-router'
+//
+// render (<Message />, document.getElementById('main'))
+//
+// console.log("Sheenan Tenepre's ECMA6imize");
